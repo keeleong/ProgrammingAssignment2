b2c9e7c0761296d17143f7dfe2b6c43269bd0fce
Tan KL <leongta@ZRHNB1202.nexus-ag.com>
2015-05-25 01:21:52 +0800

    Initial version

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,67 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment 2: Functions that cache the inverse of a matrix.
+## By: Tan KL
 
-## Write a short comment describing this function
+
+# Global variable
+# List to store cache for inverse of square matrix
+Mat_cache = list()
+
+## Function makeCacheMatrix
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  if ( nrow(x) != ncol(x) )
+  {
+    # Exception handling - X must be square matrix, else exit function 
+    print("Object is not a square matrix, exit function")
+    return
+  }
+  else
+  {
+    # X is a square matrix, proceed to cacheSolve
+    print("Object is a square matrix, proceed")
+    cacheSolve(x)
+  }
+  
 }
 
 
-## Write a short comment describing this function
+## Function cacheSolve
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix() 
+## If the inverse has already been calculated (and the matrix has not changed), then the 
+## cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+  
+  # Check if matrix doesn't change and the inverse of matrix already exists in cache
+  mat_test=matrix(1:9,nrow=3,ncol=3)
+  
+  if ( all(x == mat_test)==TRUE )
+  {
+    print("Matrix match")
+  }
+  else
+  {
+    print("Matrix doesn't match")
+    unlist(list(Mat_cache, list(x)), recursive=FALSE)
+    Mat_cache
+  }
+  return
+  
+  
+  if ( length(Mat_cache)>0 )
+  {
+    # Matrix inverse found in cache: Retrieve matrix from cache and return.
+    
+  }
+  else
+  {
+    # Matrix inverse not found in cache: compute its inverse, append to cache and return.
+    ## solve(m)
+  }
+
+  ## Return a matrix that is the inverse of 'x'
+  
 }
